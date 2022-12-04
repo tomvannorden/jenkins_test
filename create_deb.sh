d723616b2bf8c636ed6a2b2559af9d2c42a713ff
Tom van Norden <tomvannorden1@gmail.com>
2022-12-04 20:52:32 +0100

    Pushig bash script to create a .deb package

---
+++ create_deb.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#Variables
+declare version="1.0-1"
+
+#Commands
+mkdir "helloworld_${version}_arm64"
+
+mkdir -p "helloworld_${version}_arm64/hello"
+
+echo hello > ~/hello.txt
+
+cp ~/hello.txt "helloworld_${version}_arm64/hello"
+
+rm ~/hello.txt
+
+mkdir "helloworld_${version}_arm64/DEBIAN"
+
+touch "helloworld_${version}_arm64/DEBIAN/control"
+
+cat <<EOT >> "helloworld_${version}_arm64/DEBIAN/control"
+Package: hello
+Version: $version
+Architecture: amd64
+Maintainer: tnor_test
+Description: A program that greets you.
+EOT
+
+dpkg-deb --build --root-owner-group "helloworld_${version}_arm64"
+
+rm -rf "helloworld_${version}_arm64"
